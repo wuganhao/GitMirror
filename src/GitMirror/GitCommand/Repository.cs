9864448707180da4599947b6da3f90c8773dfdc8
Wu, Ganhao <wuganhao@hotmail.com>
2020-11-03 05:00:32 +0800

    Do progressive fetch

--- src/GitMirror/GitCommand/Repository.cs
+++ src/GitMirror/GitCommand/Repository.cs
@@ -319,8 +319,8 @@ namespace WuGanhao.GitMirror.GitCommand {
         public async Task MergeAsync(RemoteBranch remoteBranch) =>
             await this.ShellAsync("merge", $"remotes/{remoteBranch.Remote.Name}/{remoteBranch.Name}");
 
-        public async Task FetchAsync(string remote, string refs) => await this.ShellAsync("fetch", remote, refs);
-        public async Task FetchAsync(string remote) => await this.ShellAsync("fetch", remote);
+        public async Task FetchAsync(string remote, string refs) => await this.ShellAsync("fetch", "--progress", remote, refs);
+        public async Task FetchAsync(string remote) => await this.ShellAsync("fetch", "--progress", remote);
 
         public async Task Push (string remote, string refs) => await this.ShellAsync("push", remote, refs);
         public async Task Push (Remote remote, RemoteBranch branch) =>
