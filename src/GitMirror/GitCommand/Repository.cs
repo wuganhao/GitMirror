90c955827a79e4cafcf5a40bf3d92859d2f8b877
Wu, Ganhao <wuganhao@hotmail.com>
2020-11-04 03:31:58 +0800

    Fix - failed to push

--- src/GitMirror/GitCommand/Repository.cs
+++ src/GitMirror/GitCommand/Repository.cs
@@ -349,7 +349,7 @@ namespace WuGanhao.GitMirror.GitCommand {
         public async Task Push (string remote, string refs) => await this.ShellAsync("push", remote, refs);
         public async Task Push (Remote remote, RemoteBranch branch) =>
             await this.ShellAsync("push", remote.Name, $"refs/remotes/{branch.Remote.Name}/{branch.Name}:refs/heads/{branch.Name}");
-        public async Task Push(Remote remote, string refs) => await this.ShellAsync("push", remote.Name, refs);
+        public async Task Push(Remote remote, string refs) => await this.ShellAsync("push", remote.Name, $"refs/heads/HEAD:refs/heads/{refs}");
         public async Task CheckoutAsync(RemoteBranch originBranch) =>
             await this.ShellAsync("checkout", originBranch.FullName);
         public async Task CheckoutAsync(string branch, bool orphan = false) =>
